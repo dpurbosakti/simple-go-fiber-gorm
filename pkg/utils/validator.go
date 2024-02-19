c4a23b56aec6968b8d733a5cb41d6bc575cd461b
dpurbosakti <dwiatmokop@gmail.com>
2024-02-19 11:48:46 +0700

    add validator

---
+++ pkg/utils/validator.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// NewValidator func for create a new validator for model fields.
+func NewValidator() *validator.Validate {
+	// Create a new validator for a Book model.
+	validate := validator.New()
+
+	// Custom validation for uuid.UUID fields.
+	_ = validate.RegisterValidation("nik", nikValidator)
+
+	return validate
+}
+
+func nikValidator(fl validator.FieldLevel) bool {
+	nik := fl.Field().String()
+	regex := regexp.MustCompile(`^(1[1-9]|21|[37][1-6]|5[1-3]|6[1-5]|[89][12])\d{2}\d{2}([04][1-9]|[1256][0-9]|[37][01])(0[1-9]|1[0-2])\d{2}\d{4}$`)
+
+	result := regex.MatchString(nik)
+
+	return result
+}
+
+// ValidatorErrors func for show validation errors for each invalid fields.
+func ValidatorErrors(err error) map[string]string {
+	// Define fields map.
+	fields := map[string]string{}
+
+	// Make error message for each invalid field.
+	for _, err := range err.(validator.ValidationErrors) {
+		fields[err.Field()] = err.Error()
+	}
+
+	return fields
+}
